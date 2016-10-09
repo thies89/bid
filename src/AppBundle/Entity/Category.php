7c415e4ef4e1cb0a2616fef7718a38a7da4acb08
Marius Schneider <schneider@strg-agency.de>
2016-10-09 16:23:07 +0200

    Remove mapping

--- src/AppBundle/Entity/Category.php
+++ src/AppBundle/Entity/Category.php
@@ -2,14 +2,9 @@
 
 namespace Strassen\AppBundle\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\Common\Collections\ArrayCollection;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="category")
- */
 class Category
 {
     /**
--- src/AppBundle/Entity/Marker.php
+++ src/AppBundle/Entity/Marker.php
@@ -2,12 +2,8 @@
 
 namespace Strassen\AppBundle\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
-/**
- * @ORM\Entity
- */
 class Marker
 {
     /**
